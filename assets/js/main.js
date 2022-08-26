7aa52379a0296017be8f28e04ea7d6bcd491c4b0
Ian Marshall <ian.js.tutor@gmail.com>
2022-08-26 16:27:12 -0400

    added animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,25 @@
+import ParticleEngine from "./particle-engine.js";
+import Spline from "./splines.js";
+
+//settings
+const fgcolor = "rgba(255, 255, 255, 0.1)";
+
 //canvas
 const canvas = document.querySelector("canvas");
 canvas.width = document.body.offsetWidth;
 canvas.height = window.innerHeight;
+const ctx = canvas.getContext("2d");
+
+//setup
+Spline.setupCanvas?.(canvas);
+const simplex = new SimplexNoise(); //from <script> tag in HTML
+const engine = new ParticleEngine(ctx, {pointEvents: true, useParentForPointEvents: true});
+
+//particles
+const points = Spline.getPointsArray(canvas.width, canvas.height, {inset: 20});
+for (let {x, y} of points) {
+    engine.addParticle(new Spline(ctx, simplex, {x, y, color: fgcolor}));
+}
+
+//run
+engine.start();
